1c87ecefa10fd0834832c21adee6e21270c124ee
Francesco Lombardo <fra.lombardo92@gmail.com>
2024-05-01 18:36:27 +0200

    adding angular config

---
+++ main/neovim/.config/nvim/lua/lsp/angular.lua
@@ -0,0 +1,7 @@
+-- LSP configuration for Angular
+
+return {
+	cmd = { "ngserver", "--stdio", "--tsProbeLocations", "", "--ngProbeLocations", "" },
+	filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx" },
+}
+
